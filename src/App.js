5f0ca48cceaf8067132f5737e52f38994cd615bd
kemenike <kanayoemenike37@gmail.com>
2023-09-01 07:09:26 -0700

    Added lowlevel framework to project for different sections.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
+import "./App.css";
+import Nav from "./components/Nav";
+import Landing from "./components/Landing";
+import Specials from "./components/Specials";
+import Testimonials from "./components/Testimonials";
+import About from "./components/About";
+import Footer from "./components/Footer.tsx";
+
+import littleLemonLogo from './assets/Logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      Empty Webpage.
-    </div>
+    <>
+      <Nav logo = {littleLemonLogo}/>
+      <main>
+        <Landing />
+        <Specials />
+        <Testimonials />
+        <About />
+      </main>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Footer.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+      <img />
+      <section>
+        <h1>Doormat Navigation</h1>
+        <ul>
+          <li><a href="" role='button' className="link footer__link">Home</a></li>
+          <li><a href="" role='button' className="link footer__link">About</a></li>
+          <li><a href="" role='button' className="link footer__link">Menu</a></li>
+          <li><a href="" role='button' className="link footer__link">Reservations</a></li>
+          <li><a href="" role='button' className="link footer__link">Order Online</a></li>
+          <li><a href="" role='button' className="link footer__link">Login</a></li>
+        </ul>
+      </section>
+      <section>
+        <h1>Contact</h1>
+        <ul>
+          <li><a href="" role='button' className="link footer__link">Address</a></li>
+          <li><a href="" role='button' className="link footer__link">Phone Number</a></li>
+          <li><a href="" role='button' className="link footer__link">Email</a></li>
+        </ul>
+      </section>
+      <section>
+        <h1>Social Media Links</h1>
+        <ul>
+          <li><a href="" role='button' className="link footer__link">Facebook</a></li>
+          <li><a href="" role='button' className="link footer__link">Instagram</a></li>
+          <li><a href="" role='button' className="link footer__link">YouTube</a></li>
+          <li><a href="" role='button' className="link footer__link">Reservations</a></li>
+          <li><a href="" role='button' className="link footer__link">TikTok</a></li>
+          <li><a href="" role='button' className="link footer__link">Telegram</a></li>
+        </ul>
+      </section>
+    </>
+  )
+}
+
+export default Footer;
