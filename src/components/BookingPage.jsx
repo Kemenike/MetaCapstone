c007e746088cbfda58a5f2894c607e4ffda250b6
kemenike <kanayoemenike37@gmail.com>
2023-11-06 07:36:23 -0800

    Added framework for the useContext / createContext for the Reservation API.

--- src/components/BookingPage.jsx
+++ src/components/BookingPage.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Nav from './Nav';
 import BookingForm from './BookingForm';
 import "./BookingPage.css";
+import Reservation from './Reservation';
 
 function BookingPage() {
+
+  //TODO: Use useContext / createContext to create a reservation API.
+
+  const [reservation, setReservation] = useState({
+    date: null,
+    time: null,
+    guests: 1,
+    occasion: null
+  });
+
   return (
-    <section className="booking_page__container">
-      <section className='booking_page__col'>
-        <Nav />
-        <section className='booking_page__main'>
-          <BookingForm />
+    <Reservation.Provider value={setReservation}>
+      <section className="booking_page__container">
+        <section className='booking_page__col'>
+          <Nav />
+          <section className='booking_page__main'>
+            <BookingForm />
+          </section>
         </section>
       </section>
-    </section>
+    </Reservation.Provider>
   )
 }
 
