6a38d375a4a787c1b6af24ea36e4532783ac9e1c
kemenike <kanayoemenike37@gmail.com>
2023-12-17 15:20:19 -0800

    Formik Merge

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Nav from './Nav'
 import "./ErrorPage.css";
 import { Link } from 'react-router-dom';
 import errorImg from '../assets/errorImg.png';
@@ -8,7 +7,6 @@ import errorImg from '../assets/errorImg.png';
 function ErrorPage() {
   return (
     <section className='error__page'>
-      <Nav />
       <section className="error_page__container">
         <img src={errorImg} alt="Error Page" className="error_image" />
         <div className="error__text">
